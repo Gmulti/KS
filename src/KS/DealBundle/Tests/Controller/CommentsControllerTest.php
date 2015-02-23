18a9ca98f8c645ea289d41c41349b1f149aec0fc
Gmulti <thomasdeneulin@gmail.com>
2015-02-23 14:09:28 +0100

    comment test

--- src/KS/DealBundle/Tests/Controller/CommentsControllerTest.php
+++ src/KS/DealBundle/Tests/Controller/CommentsControllerTest.php
@@ -26,6 +26,32 @@ class CommentsControllerTest extends GeneralController
 
     }
 
+    public function testPostDealNoMedia(){
+        fwrite(STDOUT, __METHOD__ . "\n");
+        $client = static::createClient();
+
+        $crawler = $client->request('POST', 
+            '/api/v1/deals.json',
+            array(
+                'user' => self::$username,
+                'content'  => 'Contenu test'
+            ),
+            array(),
+            self::$headers
+        );
+
+        $response = $client->getResponse();
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertEquals(200, $response->getStatusCode(), 'Erreur serveur, dumper [0]["message"]');
+
+        $msg = json_decode($response->getContent(), true);
+
+        $this->assertEquals($msg['user']['username'], self::$username);
+        $this->assertEquals($msg['content'], 'Contenu test');
+
+        self::$idDeal = $msg['id'];
+    }
+
 
 
 }
